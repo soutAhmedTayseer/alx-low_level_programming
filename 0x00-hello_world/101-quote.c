b6dfab0f6b42b0db38c377e22d3c5a3444c0ca8a
soutAhmedTayseer <ahmedtayseer424@gmail.com>
2023-02-16 14:51:45 -0800

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Return: Always 1 ( Successful )
+ *
+ * Return: Always 0 ( Successful )
  */
+
 int main(void)
+
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}	
+}
