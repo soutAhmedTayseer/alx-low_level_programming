89fbdc622b6b6fde71d0eb844177dd6aa8aaa293
soutAhmedTayseer <ahmedtayseer424@gmail.com>
2023-02-16 13:36:16 -0800

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	printf("Size of a char: %lu byte(s)\n",sizeof(char));
+	printf("Size of a char: %lu byte(s)\n",sizeof(char));
+	printf("Size of a char: %lu byte(s)\n",sizeof(char));
+	printf("Size of a char: %lu byte(s)\n",sizeof(char));
+	printf("Size of a char: %lu byte(s)\n",sizeof(char));
+	return(0);
+}
